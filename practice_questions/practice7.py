aaaf84944418d6e8bbcc8dc3783a9bece4616f91
Shaik Sonia <shaiksonia222@gmail.com>
2024-08-28 01:58:24 +0530

    Python_Practice_Questions

---
+++ practice_questions/practice7.py
@@ -0,0 +1,8 @@
+# Write a program that will tell whether the number entered by the user is odd or even.
+
+num = int(input("enter a value"))
+
+if num%2==0:
+    print("even")
+else:
+    print("odd")
