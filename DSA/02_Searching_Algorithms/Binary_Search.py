d82c89aea087f58bb5494bedd7bef28e2b53ddca
Shaik Sonia <shaiksonia222@gmail.com>
2024-09-02 03:13:02 +0530

    Binary_search

--- DSA/02_Searching_Algorithms/Binary_Search.py
+++ DSA/02_Searching_Algorithms/Binary_Search.py
@@ -1,4 +1,13 @@
 #Binary Search iterative
+'''Binary search is a search algorithm used to find the position of a target value within a sorted array.
+ It works by repeatedly dividing the search interval in half until the target value is found or the interval is empty. 
+ The search interval is halved by comparing the target element with the middle value of the search space.
+
+ The Binary Search Algorithm can be implemented in the following two ways
+
+Iterative Binary Search Algorithm
+Recursive Binary Search Algorithm
+'''
 
 def binarysearch_iterative(Arr,key):
     l=0
